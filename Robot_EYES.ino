e0d32c69d9d2f04ce88daec89cd80a166d096ec0
于航 <yuahng@gmail.com>
2016-09-23 14:44:16 +0800

    Add files via upload
    云南机器人眼睛8*8点阵LED*2

---
+++ Robot_EYES.ino
@@ -0,0 +1,44 @@
+/***********************************************************/
+//Hardware: 2 x LED Matrix module
+//Board:  Arduino UNO R3
+//Function: Display EYES
+/***************************************************************/
+
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Max72xxPanel.h>
+
+int pinCS = 4; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
+int numberOfHorizontalDisplays = 1;
+int numberOfVerticalDisplays = 1;
+
+Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+
+
+void setup() {
+ matrix.fillScreen(LOW);
+ matrix.write();
+ delay(400);
+ matrix.fillScreen(HIGH);
+ matrix.write();
+ delay(400);
+
+}
+
+void loop() {
+ 
+ matrix.drawPixel(2-1, 5-2, HIGH);
+ matrix.drawPixel(3-1, 4-2, HIGH);
+ matrix.drawPixel(4-1, 4-2, HIGH);
+ matrix.drawPixel(4-1, 5-2, HIGH);
+ matrix.drawPixel(5-1, 4-2, HIGH);
+ matrix.drawPixel(5-1, 5-2, HIGH);
+ matrix.drawPixel(6-1, 4-2, HIGH);
+ matrix.drawPixel(7-1, 5-2, HIGH);
+ matrix.write(); // Send bitmap to display
+ delay(4700);
+ matrix.fillScreen(LOW);
+ matrix.write();
+ delay(300);
+}
+
